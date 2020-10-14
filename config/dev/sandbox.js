158758445022499fcd12042a49e725b40bde2e62
NguyenPhuocMinh <xiuquay55@gmail.com>
2020-10-14 23:35:33 +0700

    comment database_server

--- config/dev/sandbox.js
+++ config/dev/sandbox.js
@@ -4,6 +4,6 @@ module.exports = {
   connect: {
     enable: false,
     database_local: 'mongodb://localhost:27017/demoWhey',
-    database_server: 'mongodb+srv://npminh:12345@cluster0.pat1y.mongodb.net/accLq?retryWrites=true&w=majority'
+    // database_server: 'mongodb+srv://npminh:12345@cluster0.pat1y.mongodb.net/accLq?retryWrites=true&w=majority'
   }
 }
